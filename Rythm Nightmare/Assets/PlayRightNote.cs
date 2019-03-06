d9f3551e23ff21edbf454b9b452c467e4b47093c
Anatole <lindien.martin@gmail.com>
2019-03-06 18:31:53 +0100

    note ok

---
+++ Rythm Nightmare/Assets/PlayRightNote.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRightNote : MonoBehaviour {
+
+    public GameObject RightGameObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {     
+        if (col.gameObject == RightGameObject)
+        {
+            if (Input.GetKeyDown("right"))
+            {
+                
+                Destroy(col.gameObject);
+            }
+        }
+        
+    }
+}
